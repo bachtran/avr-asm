c1c2bb3f8c97cc03d9ea4e33c7877982e3bc5e4b
Virgil Dupras <hsoft@hardcoded.net>
2019-02-10 15:32:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# avr-asm
+
+A collection of fiddling I make with avr asm, mostly running on an arduino uno.
+So, unless otherwise noted, these projects are for atmega328p.
+
+You will notice that I don't use any external definitions, but hex codes. That's
+just me, wanting to experiment on the concept of writing code with nothing but
+the datasheet as a reference.
---
+++ common.mk
@@ -0,0 +1,26 @@
+MCU ?= atmega328p
+
+USBDEV ?= /dev/ttyACM0
+BAUDS ?= 115200
+AVRDUDEMCU ?= $(MCU)
+AVRDUDEARGS ?= -F -V -c arduino -P $(USBDEV) -b $(BAUDS)
+
+# Rules
+
+.PHONY: send clean
+
+all: $(PROGNAME).hex
+	@echo Done!
+
+send: $(PROGNAME).hex
+	avrdude $(AVRDUDEARGS) -p $(AVRDUDEMCU) -U flash:w:$(PROGNAME).hex
+
+clean:
+	rm -f $(PROGNAME).hex $(PROGNAME).bin
+
+$(PROGNAME).bin: $(PROGNAME).S
+	avr-gcc -o $@ $<
+
+$(PROGNAME).hex: $(PROGNAME).bin
+	avr-objcopy -O ihex -R .eeprom $< $@
+
